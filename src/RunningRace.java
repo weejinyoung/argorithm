967d5220857cbb353b63677b1a4d15c7990793ea
weejinyoung <a8118199@gmail.com>
2023-08-09 22:55:07 +0900

    Running race
    point: hash map

--- src/RunningRace.java
+++ src/RunningRace.java
@@ -1,11 +1,29 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class RunningRace {
-    public static void main(String[] args) {
-        ArrayList<String> players = new ArrayList<>();
-        ArrayList<String> callings = new ArrayList<>();
-        String[] a = {"mumu", "soe", "poe", "kai", "mine"};
 
+    public static void main(String[] args) {
+        String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+        String[] callings = {"kai", "kai", "mine", "mine"};
+        Map<String, Integer> playerHashSet = new HashMap<>();
+
+        int rank = 0;
+        for (String player : players) {
+            playerHashSet.put(player,rank++);
+        }
+
+        for (String calling : callings) {
+            pass(playerHashSet, players, calling);
+        }
+
+    }
+
+    static void pass(Map<String, Integer> playerHashSet, String[] players, String calledPlayer) {
+        int calledPlayerRank = playerHashSet.get(calledPlayer);
+        String aheadPlayer = players[calledPlayerRank-1];
+        playerHashSet.put(aheadPlayer, calledPlayerRank);
+        playerHashSet.put(calledPlayer, calledPlayerRank-1);
+        players[calledPlayerRank-1] = calledPlayer;
+        players[calledPlayerRank] = aheadPlayer;
     }
 }
