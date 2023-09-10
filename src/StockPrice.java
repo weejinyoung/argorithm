f9d5356556f5f4ac541dd60ca2f7d4bcab352531
weejinyoung <a8118199@gmail.com>
2023-09-10 22:21:29 +0900

    Stock price

---
+++ src/StockPrice.java
@@ -0,0 +1,21 @@
+class StockPrice {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for (int j = 0; j < prices.length; j++) {
+            answer[j] = prices.length - 1 - j;
+        }
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int k = i + 1; k < prices.length; k++) {
+                if (prices[i] > prices[k]) {
+                    answer[i] = k - i;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+
+    }
+}
+
