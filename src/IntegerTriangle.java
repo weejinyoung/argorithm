3b10a7d36ab967fe984b9732fa93010e6556dad0
weejinyoung <a8118199@gmail.com>
2023-10-12 17:58:28 +0900

    Integer triangle

--- src/IntegerTriangle.java
+++ src/IntegerTriangle.java
@@ -2,9 +2,8 @@ public class IntegerTriangle {
 
     public int solution(int[][] triangle) {
         for (int i = triangle.length - 2; i >= 0; i--) {
-            int[] row = triangle[i];
-            for (int j = 0; j < row.length; j++) {
-                row[j] = Math.max(row[j]+ triangle[i+1][j], row[j] + triangle[i+1][j+1]);
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] = Math.max(triangle[i][j]+ triangle[i+1][j], triangle[i][j] + triangle[i+1][j+1]);
             }
         }
         return triangle[0][0];
