fa4d82e1fd0ba94d3e2e078189c73924cf312a7a
weejinyoung <a8118199@gmail.com>
2023-10-24 22:16:42 +0900

    Process

---
+++ src/Process.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Process {
+
+    public int solution(int[] priorities, int location) {
+
+        Map<Integer, Integer> processMap = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            processMap.put(i, priorities[i]);
+            pq.add(priorities[i]);
+            q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+            if (!processMap.get(current).equals(pq.peek())) {
+                q.add(current);
+            } else {
+                pq.poll();
+                answer.add(current);
+            }
+        }
+
+        return (answer.indexOf(location) + 1);
+    }
+}
