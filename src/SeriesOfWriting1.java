d40098c924c031d7538047a82ffb5b3c2d18bb37
weejinyoung <a8118199@gmail.com>
2023-11-30 16:07:00 +0900

    Series of writing

---
+++ src/SeriesOfWriting1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SeriesOfWriting1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int cnt = 1;
+        int len = 10;
+
+        for (int i = 1; i <= N; i++) {
+            if (i == len) {
+                len *= 10;
+                cnt++;
+            }
+            answer += cnt;
+        }
+        System.out.println(answer);
+
+    }
+}
