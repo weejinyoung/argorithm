ed05b75d0c2e5576ac097115837c7819ac56850b
weejinyoung <a8118199@gmail.com>
2024-01-26 22:28:37 +0900

    백준 123 더하기 3

---
+++ src/백준/백준123더하기3.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준123더하기3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 1000001; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
