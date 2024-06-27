51ad2c5a07c60f50810c2d06ac1e0720c471d149
weejinyoung <a8118199@gmail.com>
2024-06-27 16:37:55 +0900

    refact: 로직 단순화

---
+++ src/유레카/a0627greedy/BJ1992.java
@@ -0,0 +1,8 @@
+package 유레카.a0627greedy;
+
+public class BJ1992 {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/유레카/a0627greedy/BJ2630.java
+++ src/유레카/a0627greedy/BJ2630.java
@@ -11,35 +11,23 @@ public class BJ2630 {
     static int white = 0;
     static int blue = 0;
 
-    static void countPaper() {
-
-    }
-
-    static boolean search(int standardI, int standardJ, int length) {
-        int standard = map[standardI][standardJ];
-        for (int i = standardI; i < standardI + length; i++) {
-            for (int j = standardJ; j < standardJ + length; j++) {
-                if(map[i][j] != standard) return false;
+    static boolean isCovered(int si, int sj, int l) {
+        for (int i = si; i < si + l; i++) {
+            for (int j = sj; j < sj + l; j++) {
+                if (map[i][j] != map[si][sj]) return false;
             }
         }
-        if (standard == 1) blue++;
+        if (map[si][sj] == 1) blue++;
         else white++;
         return true;
     }
 
-    static void recursion(int standardI, int standardJ, int length) {
-        if(length == 1) {
-            if (map[standardI][standardJ] == 1) blue++;
-            else white++;
-            return;
-        }
-
-        if (search(standardI, standardJ, length)) return;
-
-        recursion(standardI, standardJ, length / 2);
-        recursion(standardI + (length / 2), standardJ, length / 2);
-        recursion(standardI , standardJ + (length / 2), length / 2);
-        recursion(standardI + (length / 2), standardJ + (length / 2), length / 2);
+    static void recursion(int si, int sj, int l) {
+        if (isCovered(si, sj, l)) return;
+        recursion(si, sj, l / 2);
+        recursion(si + (l / 2), sj, l / 2);
+        recursion(si, sj + (l / 2), l / 2);
+        recursion(si + (l / 2), sj + (l / 2), l / 2);
     }
 
     public static void main(String[] args) throws IOException {
@@ -47,9 +35,9 @@ public class BJ2630 {
         int N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         StringTokenizer st;
-        for (int i = 0; i < N ; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N ; j++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -58,3 +46,4 @@ public class BJ2630 {
         System.out.println(blue);
     }
 }
+
